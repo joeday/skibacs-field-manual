a6db8cdaadede01806b3f41ef8da52cba14d614e
joeday <thejode@gmail.com>
2023-12-19 20:15:58 -0800

    ACTUALLY fix import statements

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Card from "../app/ui/card";
+import Card from "@/app/ui/card";
 
 export default function Home() {
   return (
